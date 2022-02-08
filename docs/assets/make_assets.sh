8f02d7b4052283bf98af58cc0cc11d4853f7a3e9
Cory McCartan <cory.mccartan@gmail.com>
2022-02-08 01:12:22 -0500

    50-state logo

--- docs/assets/make_assets.sh
+++ docs/assets/make_assets.sh
@@ -17,4 +17,13 @@ convert -background white -gravity center alarm_256_wh.png -resize 256x256 -exte
 convert -background white -gravity center alarm_64_wh.png -resize 64x64 -extent 64x64 alarm_64_wh.png
 convert -background none -gravity center favicon.png -resize 64x64 -extent 64x64 favicon.png
 convert -background white -gravity center alarm_1200_crop.png -resize 1200x1200 -extent 1200x1200 alarm_1200_crop.png
+
+rsvg-convert -h 1200 fifty_states.svg          > fifty_states_1200_tr.png
+rsvg-convert -h 1200 fifty_states.svg -b white > fifty_states_1200_wh.png
+rsvg-convert -h 256 fifty_states.svg          > fifty_states_256_tr.png
+rsvg-convert -h 256 fifty_states.svg -b white > fifty_states_256_wh.png
+convert -background none -gravity center fifty_states_1200_tr.png -resize 1200x1200 -extent 1200x1200 fifty_states_1200_tr.png
+convert -background none -gravity center fifty_states_256_tr.png -resize 256x256 -extent 256x256 fifty_states_256_tr.png
+convert -background white -gravity center fifty_states_1200_wh.png -resize 1200x1200 -extent 1200x1200 fifty_states_1200_wh.png
+convert -background white -gravity center fifty_states_256_wh.png -resize 256x256 -extent 256x256 fifty_states_256_wh.png
 echo "Done."
