818db5b8a01ced5609db529cdf50317921a766d9
Christopher Kenny <ctk44@cornell.edu>
2023-06-17 00:32:20 -0400

    add fn for website

--- _fifty-states/fifty-states.R
+++ _fifty-states/fifty-states.R
@@ -205,3 +205,37 @@ render_state_page = function(states, quiet=TRUE) {
         cat("Rendered `", slug, "`\n", sep="")
     })
 }
+
+list_elections <- function(tb) {
+    elecs <- tb |>
+        dplyr::as_tibble() |>
+        dplyr::select(tidyselect::matches('_\\d\\d_')) |>
+        colnames() |>
+        stringr::str_sub(start = 1, end = 6) |>
+        unique() |>
+        stringr::str_replace(pattern = '_', replacement = ' ')
+
+    el_expanded <- dplyr::tibble(
+        a = stringr::str_sub(string = elecs, start = 1, end = 3)
+    )
+    el_expanded <- dplyr::left_join(
+        x = el_expanded, y = alarm_abb(), by = 'a'
+    )
+
+    for (i in seq_along(elecs)) {
+        elecs[i] <- paste0(el_expanded$b[i], ' 20', stringr::str_sub(elecs[i], start = 5))
+    }
+
+    elecs
+}
+
+alarm_abb <- function() {
+    tibble::tribble(
+        ~a, ~b,
+        'pre', 'President',
+        'uss', 'US Senate',
+        'gov', 'Governor',
+        'atg', 'Attorney General',
+        'sos', 'Secretary of State'
+    )
+}
